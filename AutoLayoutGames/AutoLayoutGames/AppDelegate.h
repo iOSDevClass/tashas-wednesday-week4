90391c2976b69d61beb07654f201905aa62707ce
Tatiana Jamison <jamisont@vt.edu>
2015-10-21 13:53:10 -0700

    Challenge #2
    Configure image view, label, and button to "look good" on iPhone 6 Plus and iPhone 4s

---
+++ AutoLayoutGames/AutoLayoutGames/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutGames
+//
+//  Created by Tatiana Jamison on 10/21/15.
+//  Copyright © 2015 Bit Lore Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayoutGames/AutoLayoutGames/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutGames
+//
+//  Created by Tatiana Jamison on 10/21/15.
+//  Copyright © 2015 Bit Lore Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
